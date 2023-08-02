8527f23f8dbfe4c8b2af691445eb51ba38f93bec
howco-dvdreedy <david.reedy@howcogroup.com>
2023-08-02 10:53:48 -0500

    fix base name

--- src/providers/app.tsx
+++ src/providers/app.tsx
@@ -40,7 +40,7 @@ export const AppProvider = ({ children }: AppProviderProps) => {
                   {/* {process.env.NODE_ENV !== 'test' && <ReactQueryDevtools />}
             <Notifications />
             <AuthProvider> */}
-                  <Router basename={import.meta.env.DEV ? '/' : '/react-vite-gh-pages/'}>{children}</Router>
+                  <Router basename={import.meta.env.DEV ? '/' : '/React-Template/'}>{children}</Router>
                   {/* </AuthProvider> */}
                   {/* </QueryClientProvider> */}
                   {/* </HelmetProvider> */}
